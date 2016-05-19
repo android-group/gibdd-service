c32fb7a8b552b03178528f8cccce4a9545f91aa5
olga <o.taskaeva@pflb.ru>
2016-05-19 16:10:20 +0300

    Актуализирован интерфейс

--- app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
@@ -2,7 +2,6 @@ package ru.android_studio.olga.gibdd_servis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
