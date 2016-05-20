4b403138a1ad2ebeee5382e7c7548446c5d084cd
Yury Andreev <andreevym@gmail.com>
2016-05-20 03:05:48 +0300

    Страница Водителя, доработка главной убрал надпись - позволило сделать для разных разрешений, т.к в горизонтальном режиме на телефоне просмотр не возможен
    фиксированный размер иконок на главной
    
    https://github.com/android-group/gibdd-servis/issues/3
    https://github.com/android-group/gibdd-servis/issues/2

---
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/DriverActivity.java
@@ -0,0 +1,31 @@
+package ru.android_studio.olga.gibdd_servis;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DriverActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "DriverActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_my);
+        setSupportActionBar(toolbar);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        supportActionBar.setDisplayShowTitleEnabled(true);
+        supportActionBar.setIcon(R.mipmap.driver_logo);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
@@ -1,10 +1,15 @@
 package ru.android_studio.olga.gibdd_servis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_my);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.mipmap.main_logo);
 
-        toolbar.setLogo(R.mipmap.logo);
+        findViewById(R.id.button_driver).setOnClickListener(this);
+        findViewById(R.id.button_car).setOnClickListener(this);
+        findViewById(R.id.button_fine).setOnClickListener(this);
+        findViewById(R.id.button_message).setOnClickListener(this);
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_fine:
+                Log.i(TAG, "button_fine");
+                break;
+            case R.id.button_driver:
+                Log.i(TAG, "button_driver");
+                Intent intentDriverActivity = new Intent(this, DriverActivity.class);
+                startActivity(intentDriverActivity);
+                break;
+            case R.id.button_car:
+                Log.i(TAG, "button_car");
+                break;
+            case R.id.button_message:
+                Log.i(TAG, "button_message");
+                break;
+        }
     }
 }
