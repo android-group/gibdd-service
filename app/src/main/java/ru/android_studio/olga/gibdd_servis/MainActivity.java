35c29a504e64f9946f6274b261531a247d4bea77
Ruslan Suleymanov <rusdenis@rambler.ru>
2016-07-01 03:54:27 +0300

    OCR Service (Tesseract)
    Сервис Tesseract OCR переделан в одиночку (Singleton). Добавлена потокозащищенность (частично).
    Подготовка экземпляра Tesseract производится только один раз, при этом для каждого языка должен быть свой подготовленный API экземпляр.
    В активити инициализация экземпляра OCR сервиса производится в фоновом процессе.

--- app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
@@ -1,7 +1,15 @@
 package ru.android_studio.olga.gibdd_servis;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.io.IOException;
+
+import ru.android_studio.olga.gibdd_servis.service.OCRService;
+import ru.android_studio.olga.gibdd_servis.service.imp.TesseractOCRServiceImp;
 
 public class MainActivity extends ActivityWithMenu {
 
@@ -17,6 +25,25 @@ public class MainActivity extends ActivityWithMenu {
         getSupportActionBar().setLogo(R.mipmap.main_logo);
 
         setMenuConfig();
+
+        PrepareOCRAsyncTask ocrAsyncTask = new PrepareOCRAsyncTask();
+        try {
+            ocrAsyncTask.execute(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            TesseractOCRServiceImp.getInstance().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Error by Tesseract OCR service closing", e);
+        }
+        super.onDestroy();
     }
 
 
@@ -24,4 +51,16 @@ public class MainActivity extends ActivityWithMenu {
     int getCurrentMenuId() {
         return 0;
     }
+
+    private class PrepareOCRAsyncTask extends AsyncTask<Context, Void, OCRService> {
+
+        @Override
+        protected OCRService doInBackground(Context... params) {
+            OCRService service = TesseractOCRServiceImp.getInstance(params[0]);
+            service.prepare();
+            return service;
+        }
+
+    }
+
 }
