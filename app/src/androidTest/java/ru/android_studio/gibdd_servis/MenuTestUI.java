23c50027ba34a4fef69245344977a0c283e876a9
Yury Andreev <andreevym@gmail.com>
2016-07-25 00:34:09 +0300

    запрос "Проверка автомобиля"

---
+++ app/src/androidTest/java/ru/android_studio/gibdd_servis/MenuTestUI.java
@@ -0,0 +1,68 @@
+package ru.android_studio.gibdd_servis;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import ru.android_studio.gibdd_servis.car.activity.CarActivity;
+import ru.android_studio.gibdd_servis.pageObject.MenuPage;
+
+/**
+ * Тестирование переходов по меню
+ */
+public class MenuTestUI extends TestUI<MainActivity> {
+
+    public MenuTestUI() {
+    }
+
+    @Rule
+    public ActivityTestRule<CarActivity> carActivityTestRule = new ActivityTestRule<>(CarActivity.class);
+
+    /**
+     * Начиная с кнопки "Проверка автомобиля"
+     */
+    @Test
+    public void testMenuCarBtn() {
+        menuPage.clickMenuCarBtn();
+        new MenuPage(carActivityTestRule.getActivity()).clickMenuDriverBtn();
+        //menuPage.clickMenuDriverBtn();
+        //menuPage.clickMenuFineBtn();
+        //menuPage.clickMenuQuestionBtn();
+    }
+
+    /*
+
+    *//**
+     * Начиная с кнопки "Проверка водителя"
+     * *//*
+    @Test
+    public void testMenuMenuDriverBtn() {
+        menuPage.clickMenuDriverBtn();
+        menuPage.clickMenuFineBtn();
+        menuPage.clickMenuQuestionBtn();
+        menuPage.clickMenuCarBtn();
+    }
+
+    *//**
+     * Начиная с кнопки "Проверка штрафов"
+     * *//*
+    @Test
+    public void testMenuFromMenuFineBtn() {
+        menuPage.clickMenuFineBtn();
+        menuPage.clickMenuQuestionBtn();
+        menuPage.clickMenuCarBtn();
+        menuPage.clickMenuDriverBtn();
+    }
+
+    *//**
+     * Начиная с кнопки "Прием обращений"
+     * *//*
+    @Test
+    public void testMenuFromMenuQuestionBtn() {
+        menuPage.clickMenuQuestionBtn();
+        menuPage.clickMenuCarBtn();
+        menuPage.clickMenuDriverBtn();
+        menuPage.clickMenuFineBtn();
+    }*/
+}
