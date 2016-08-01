899db04c195d536db3ee7b577bf917f09f0440d3
Ruslan Suleymanov <rusdenis@rambler.ru>
2016-08-02 00:58:03 +0300

    OCR Service (Tesseract)
    В старом коде (до отпуска) коммит, где убрал обращение к OCR Service в MainActivity.
    Нужно сравнить с текущей версией в master и решить что делать с этими изменениями.
    В будущем нужно выносить код в службу Android.

--- app/src/main/java/ru/android_studio/olga/gibdd_servis/BootApplication.java
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/BootApplication.java
@@ -1,14 +1,28 @@
 package ru.android_studio.olga.gibdd_servis;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import ru.android_studio.olga.gibdd_servis.service.OCRService;
+import ru.android_studio.olga.gibdd_servis.service.imp.TesseractOCRServiceImp;
 
 /*
 * При загрузке устанавливаем в Picasso кэш OkHttpDownloader
 * */
 public class BootApplication extends Application {
+
+    private static final String TAG = BootApplication.class.getSimpleName();
+
+    private static OCRService ocrService;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -20,5 +34,36 @@ public class BootApplication extends Application {
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
 
+        PrepareOCRAsyncTask ocrAsyncTask = new PrepareOCRAsyncTask();
+        try {
+            ocrAsyncTask.execute(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage(), e);
+        }
     }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        if (ocrService instanceof Closeable)
+            try {
+                ((Closeable) ocrService).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Error by OCR service closing", e);
+            }
+    }
+
+    private class PrepareOCRAsyncTask extends AsyncTask<Context, Void, OCRService> {
+
+        @Override
+        protected OCRService doInBackground(Context... params) {
+            ocrService = TesseractOCRServiceImp.getInstance(params[0]);
+            ocrService.prepare();
+            return ocrService;
+        }
+
+    }
+
 }
--- app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
+++ app/src/main/java/ru/android_studio/olga/gibdd_servis/MainActivity.java
@@ -1,18 +1,11 @@
 package ru.android_studio.olga.gibdd_servis;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 
 import butterknife.ButterKnife;
-import ru.android_studio.olga.gibdd_servis.service.OCRService;
-import ru.android_studio.olga.gibdd_servis.service.imp.TesseractOCRServiceImp;
 
 public class MainActivity extends ActivityWithMenu {
 
-    public static TesseractOCRServiceImp ocrService;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -26,46 +19,16 @@ public class MainActivity extends ActivityWithMenu {
         setSupportActionBar(toolbar);
 
         setMenuConfig();
-
-        if (ocrService == null) {
-            PrepareOCRAsyncTask ocrAsyncTask = new PrepareOCRAsyncTask();
-            try {
-                ocrAsyncTask.execute(this);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e(TAG, e.getMessage(), e);
-            }
-        }
     }
 
     @Override
     protected void onDestroy() {
-        if (ocrService != null) {
-            try {
-                ocrService.close();
-            } catch (Throwable e) {
-                e.printStackTrace();
-                Log.e(TAG, "Error by Tesseract OCR service closing", e);
-            }
-        }
         super.onDestroy();
     }
-
 
     @Override
     protected int getCurrentMenuId() {
         return 0;
-    }
-
-    private class PrepareOCRAsyncTask extends AsyncTask<Context, Void, OCRService> {
-
-        @Override
-        protected OCRService doInBackground(Context... params) {
-            ocrService = TesseractOCRServiceImp.getInstance(params[0]);
-            ocrService.prepare();
-            return ocrService;
-        }
-
     }
 
 }
